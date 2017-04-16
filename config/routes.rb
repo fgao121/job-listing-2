5795f1d1035dfeccbdec689a172858a97ca62cce
fei <fgao121@gmail.com>
2017-04-16 11:10:08 +0800

    step6 order by wage

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ Rails.application.routes.draw do
   resources :jobs
   namespace :admin do
     resources :jobs
-      member do
-        post :publish
-        post :hide   
   end
+  namespace :admin do
+   resources :jobs do
+    member do
+      post :publish
+      post :hide
+    end
+   end
+  end
+
   root 'jobs#index'
 end
