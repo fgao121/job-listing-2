bd2c13bc1d0dd7b5e664b3815d1427db1357f8f2
fei <fgao121@gmail.com>
2017-04-20 10:39:20 +0800

    add search function to navbar

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  before_action :validate_search_key, only: [:search]
 
   def index
     @jobs = case params[:order]
@@ -56,6 +57,25 @@ class JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+  end
+
+
+  def search
+    if @query_string.present?
+      search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 
 end
